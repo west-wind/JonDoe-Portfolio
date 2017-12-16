03928575092a7aec3d942286f1b05e5a7f4f0962
west-wind <alexjon@zoho.com>
2017-12-16 14:02:36 +0300

    Primary 001

---
+++ js/resumeBuilder.js
@@ -0,0 +1,207 @@
+//Personal Information Object
+var bio = {
+	"name":"Jonathan Doe",
+	"profession":" / Information Security Specialist",
+	"contacts" : {"mobile": "+1 347 01234567", "email": "alexjon@zoho.com", "github": "west-wind", "location": "Manhattan, NY", "twitter": "@JonathanDoe"},
+	"welcome":"Shell is only the begining",
+	"skills": ["Penetration Testing", "Firewall Configuration & Administration", "Malware Analysis", "Implementing Sandboxing", "Implementing Proxy"],
+	"biopic": "images/jonathan.jpg"
+};
+
+//Education History Object
+var education = {
+	"schools": 
+	[
+	{"name":"Middlesex University", "city":"London, United Kingdom", "degree":"M.Sc. Cybersecurity", "dates": "2014 - 2015", "major": ["Computer Security", " Network Security"]}, 
+	{"name":"Karunya University", "city":"Coimbatore, India", "degree":"B.Tech. Computer Science & Engineering", "dates": "2008 - 2012", "major": ["Computer Science", " Cryptography"]}
+	],
+	"onlineCourses":
+	[
+	{"name": "Javascript Basics", "school": "Udacity", "dates": 2017, "url": "www.udacity.com"},
+	{"name": "Metsaploit", "school": "Cybrary IT", "dates": 2017, "url": "www.cybrary.it"}
+	]
+};
+
+//Work History Object
+var work = {
+	"jobs":
+	[
+	{"employer":"Raytheon", "title":"IT Security Administrator", "dates": "January 2017 - Present", "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ante in nibh mauris cursus mattis molestie a. Tortor condimentum lacinia quis vel eros donec ac odio. Pellentesque elit ullamcorper dignissim cras. At quis risus sed vulputate odio. Velit sed ullamcorper morbi tincidunt ornare massa. Amet purus gravida quis blandit turpis cursus in. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a. Aliquet sagittis id consectetur purus ut faucibus. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Viverra vitae congue eu consequat ac felis. Accumsan in nisl nisi scelerisque eu ultrices vitae. Varius duis at consectetur lorem. Tempus quam pellentesque nec nam." },
+	{"employer":"Lockheed Martin - Skunk Works", "title":"Network Security Engineer", "dates": "January 2016 - December 2016", "description":"Nibh venenatis cras sed felis. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Lobortis scelerisque fermentum dui faucibus in. Lectus urna duis convallis convallis tellus id. Sed vulputate mi sit amet mauris commodo quis imperdiet. Nibh sed pulvinar proin gravida hendrerit lectus a. Arcu dui vivamus arcu felis bibendum. Leo duis ut diam quam. Mauris a diam maecenas sed enim. In fermentum et sollicitudin ac. Ipsum consequat nisl vel pretium. Egestas dui id ornare arcu odio ut sem. Sodales ut eu sem integer vitae justo eget magna fermentum. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor. Pretium viverra suspendisse potenti nullam ac. Scelerisque eu ultrices vitae auctor eu."},
+	{"employer":"IBM", "title":"Network Security Specialist", "dates": "January 2015 - December 2015", "description": "Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Eu volutpat odio facilisis mauris sit amet massa vitae. Velit scelerisque in dictum non consectetur a erat nam at. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Lobortis feugiat vivamus at augue eget. Id velit ut tortor pretium viverra. Amet nisl purus in mollis. Semper viverra nam libero justo laoreet sit amet. Mauris augue neque gravida in fermentum et. Nibh cras pulvinar mattis nunc sed blandit libero volutpat sed. Suspendisse faucibus interdum posuere lorem. Velit scelerisque in dictum non consectetur a erat. Tempus quam pellentesque nec nam aliquam. Amet justo donec enim diam vulputate. Quis commodo odio aenean sed. Orci a scelerisque purus semper eget duis at tellus at. Metus vulputate eu scelerisque felis imperdiet proin."},
+	{"employer":"Apple Inc.", "title":"Associate Cybersecurity Analyst", "dates": "January 2014 - December 2014", "description":"Odio euismod lacinia at quis risus sed vulputate. Vitae justo eget magna fermentum iaculis eu. Facilisis sed odio morbi quis commodo odio aenean sed adipiscing. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique. Pretium lectus quam id leo. Consequat semper viverra nam libero. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Quis enim lobortis scelerisque fermentum dui faucibus in. Facilisis gravida neque convallis a. Ante metus dictum at tempor commodo ullamcorper a lacus. Eu ultrices vitae auctor eu augue. Malesuada proin libero nunc consequat. Arcu vitae elementum curabitur vitae nunc sed velit dignissim. Massa placerat duis ultricies lacus. Morbi quis commodo odio aenean sed adipiscing diam donec. Lectus arcu bibendum at varius vel pharetra. Donec pretium vulputate sapien nec sagittis aliquam malesuada. Eget gravida cum sociis natoque penatibus."}
+	]
+};
+
+//Project Details Object
+var project = {
+	"projects": 
+	[
+	{"title":"MOI KAP-1", "dates": "January 2017 - December 2017", "description": "Neque egestas congue quisque egestas diam in arcu. Risus in hendrerit gravida rutrum quisque non. Duis at consectetur lorem donec massa. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Risus nullam eget felis eget nunc lobortis. Adipiscing at in tellus integer feugiat scelerisque varius morbi enim. Eu facilisis sed odio morbi quis. Nunc faucibus a pellentesque sit amet porttitor. Risus pretium quam vulputate dignissim suspendisse. Ultrices dui sapien eget mi proin sed. Purus faucibus ornare suspendisse sed nisi lacus sed." },
+	{"title":"TVTC", "dates": "January 2016 - December 2016", "description": "Bibendum at varius vel pharetra vel turpis nunc eget. Eget mauris pharetra et ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Senectus et netus et malesuada fames ac turpis. Viverra justo nec ultrices dui sapien eget mi proin. Quam nulla porttitor massa id neque aliquam vestibulum. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Tortor consequat id porta nibh. Auctor neque vitae tempus quam. Quisque id diam vel quam elementum pulvinar etiam. Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat. Condimentum mattis pellentesque id nibh tortor id aliquet. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Pulvinar etiam non quam lacus suspendisse faucibus. Dui accumsan sit amet nulla facilisi morbi tempus."}
+	]
+};
+
+function displayBio()
+{
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedProfession = HTMLheaderRole.replace("%data%", bio.profession);
+	var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+	var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+	$("#header").append(formattedName);
+	$("#header").append(formattedProfession);
+	$("#header").append(formattedPic);
+	$("#header").append(formattedWelcome);
+	$("#header").append(formattedMobile);
+	$("#header").append(formattedEmail);
+	$("#header").append(formattedGithub);
+	$("#header").append(formattedLocation);
+	$("#header").append(formattedTwitter);
+}
+displayBio();
+
+function displaySkills()
+{
+ if(bio.skills.length > 0)
+ {
+	var formattedSkill;
+
+	$("#header").append(HTMLskillsStart); //Displays 'Skills at a glace' text
+	
+	formattedSkill = HTMLskills.replace("%data%",bio.skills[0]);
+	$("#skills").append(formattedSkill);
+	console.log(bio.skills[0]);
+
+	formattedSkill = HTMLskills.replace("%data%",bio.skills[1]);
+	$("#skills").append(formattedSkill);
+	console.log(bio.skills[1]);
+
+	formattedSkill = HTMLskills.replace("%data%",bio.skills[2]);
+	$("#skills").append(formattedSkill);
+	console.log(bio.skills[2]);
+
+	formattedSkill = HTMLskills.replace("%data%",bio.skills[3]);
+	$("#skills").append(formattedSkill);
+	console.log(bio.skills[3]);
+
+	//formattedSkill = HTMLskills.replace("%data%",bio.skills[4]);
+	//$("#skills").append(formattedSkill);
+	//console.log(bio.skills[4]);
+ }
+}
+displaySkills();
+
+function displayWork()
+{
+	for (job in work.jobs)
+	{
+	$("#workExperience").append(HTMLworkStart);
+    //Display Job Title & Employer
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	$(".work-entry:last").append(formattedEmployerTitle);
+    //Display Job Dates
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedDates);
+	//Display Work Description
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+    }
+}
+displayWork();
+
+project.display = function()
+{
+	for (element in project.projects)
+	{
+	$("#projects").append(HTMLprojectStart);
+	
+	//Display Projects Title
+	var formattedTitle = HTMLprojectTitle.replace("%data%", project.projects[element].title);
+	$(".project-entry:last").append(formattedTitle);
+    
+    //Display Project Dates
+	var formattedDates = HTMLprojectDates.replace("%data%", project.projects[element].dates);
+	$(".project-entry:last").append(formattedDates);
+    
+    //Display Project Description
+	var formattedDescription = HTMLprojectDescription.replace("%data%", project.projects[element].description);
+	$(".project-entry:last").append(formattedDescription);
+    }
+}
+project.display();
+
+function displayEducation()
+{
+	for (elements in education.schools)
+	{
+	$("#education").append(HTMLschoolStart);
+    //Display School Name & Degree
+	var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[elements].name);
+	var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[elements].degree);
+	var formattedSchoolTitle = formattedschoolName + formattedschoolDegree;
+	$(".education-entry:last").append(formattedSchoolTitle);
+    
+    //Display School Dates
+	var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[elements].dates);
+	$(".education-entry:last").append(formattedschoolDates);
+
+	//Display School City
+	var formattedschoolCity = HTMLschoolLocation.replace("%data%", education.schools[elements].city);
+	$(".education-entry:last").append(formattedschoolCity);
+
+	//Display School Major
+	var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[elements].major);
+	$(".education-entry:last").append(formattedschoolMajor);
+    }
+}
+displayEducation();
+
+/*$("#education").append(HTMLonlineClasses); 
+function displayOnlineEducation()
+{
+	for (elementium in education.onlineCourses)
+	{
+	$("#education").append(HTMLschoolStart);
+	//Display School Name & Degree
+	var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[elementium].name);
+	var formattedOnlineName = HTMLonlineSchool.replace("%data%", education.onlineCourses[elementium].school);
+	var formattedonlineSchoolTitle = formattedOnlineTitle + formattedOnlineName;
+	$(".education-entry:last").append(formattedonlineSchoolTitle);
+    
+    //Display School Dates
+	var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[elementium].dates);
+	$(".education-entry:last").append(formattedOnlineDates);
+
+	//Display School URL
+	//var formattedschoolCity = HTMLschoolLocation.replace("%data%", education.schools[elements].city);
+	//$(".education-entry:last").append(formattedschoolCity);
+
+	//Display School Major
+	//var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[elements].major);
+	//$(".education-entry:last").append(formattedschoolMajor);
+    }
+}
+displayOnlineEducation();
+*/
+
+$("#mapDiv").append(googleMap);
+
+
+/*
+This is empty on purpose! Your code to build the resume will go here.
+//Playing with Arrays
+//console.log("\nPlaying with Arrays:");
+//var skills = ["Awesomeness ", "Information Security ","ISO 270001 Gap Assessment "];
+//$("#main").append(skills);
+//$("#main").append(skills.length);
+/*
+$("#main").append(work["currentPosition"]);
+$("#main").append(education.university);
+*/
